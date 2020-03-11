f45f97ab2f5ccfc590ebd0969342163bbc76ba93
Vlad <vladoseyev3@gmail.com>
2020-03-11 14:13:52 -0700

    Updates

---
+++ precache-manifest.7a66c3c87ca9602f7cdf410557d72549.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2656dc300e59fdfd7e935581d4b892e9",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "345dc98a0345caf1fe88",
+    "url": "/FaceRecognition/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "36b8a39ae507533399b6",
+    "url": "/FaceRecognition/static/css/main.42839c49.chunk.css"
+  },
+  {
+    "revision": "345dc98a0345caf1fe88",
+    "url": "/FaceRecognition/static/js/2.d793ee96.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/FaceRecognition/static/js/2.d793ee96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b8a39ae507533399b6",
+    "url": "/FaceRecognition/static/js/main.7f2b464c.chunk.js"
+  },
+  {
+    "revision": "d342bbe3ecab1deccf87",
+    "url": "/FaceRecognition/static/js/runtime-main.98a46ff4.js"
+  }
+]);
